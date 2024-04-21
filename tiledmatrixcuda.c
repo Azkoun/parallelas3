893ff3fcfa10099026192740341fb105b12673dd
Azkoun <108796789+Azkoun@users.noreply.github.com>
2024-04-21 17:26:04 +0300

    Create tiledmatrixcuda.c

---
+++ tiledmatrixcuda.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define TILE_WIDTH 16
+
+__global__ void matrixMulTiled(float *A, float *B, float *C, int width) {
+    __shared__ float As[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float Bs[TILE_WIDTH][TILE_WIDTH];
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tx = threadIdx.x, ty = threadIdx.y;
+    int Row = by * TILE_WIDTH + ty;
+    int Col = bx * TILE_WIDTH + tx;
+    float Cvalue = 0;
+    for (int m = 0; m < (width / TILE_WIDTH); ++m) {
+        As[ty][tx] = A[Row * width + m * TILE_WIDTH + tx];
+        Bs[ty][tx] = B[(m * TILE_WIDTH + ty) * width + Col];
+        __syncthreads();
+        for (int k = 0; k < TILE_WIDTH; ++k) {
+            Cvalue += As[ty][k] * Bs[k][tx];
+        }
+        __syncthreads();
+    }
+    C[Row * width + Col] = Cvalue;
+}
+
+int main() {
+    int width = 1024; // Define the size of the matrix
+    size_t size = width * width * sizeof(float);
+    float *h_A, *h_B, *h_C;
+    float *d_A, *d_B, *d_C;
+
+
+    h_A = (float*)malloc(size);
+    h_B = (float*)malloc(size);
+    h_C = (float*)malloc(size);
+
+    // Initialize host matrices
+    randomInit(h_A, width * width);
+    randomInit(h_B, width * width);
+
+    cudaMalloc((void**)&d_A, size);
+    cudaMalloc((void**)&d_B, size);
+    cudaMalloc((void**)&d_C, size);
+
+
